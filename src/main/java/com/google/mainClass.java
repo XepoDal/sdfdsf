f4c83792c9d0fb67ce06914001183a2b0155d4f4
XepoDal <37509521+XepoDal@users.noreply.github.com>
2018-06-15 17:07:00 +0300

    Copy

---
+++ src/main/java/com/google/mainClass.java
@@ -0,0 +1,18 @@
+package com.google;
+
+
+
+public class mainClass {
+
+    public static void main(String [] args) {
+
+        /*System.out.println("Hallo");
+        testClass testClass = new testClass();
+        try {
+            testClass.test();
+        } catch (InterruptedException e) {
+            System.out.println("test 0 не запустился");
+        }*/
+    }
+
+}
